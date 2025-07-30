9e6ddf53ffa23265b3191e0d8da323d4a331cb5b
Nathan Tihon <nathan.tihon@uclouvain.be>
2025-07-30 11:35:03 +0200

    Load only ndoes from 2D entities from GMSH

--- test_WNOcTree.py
+++ test_WNOcTree.py
@@ -8,12 +8,12 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import igl
 import gmsh
 
-plot = True
+plot = False
 
 gmsh.initialize()
 gmsh.option.setNumber("Mesh.MeshSizeFactor", 0.2)
 
-n = 100
+n = 20
 s = 0.2    # shift parameter
 
 # Generate two circles 
@@ -22,7 +22,7 @@ right_sphere = gmsh.model.occ.addSphere(0.5-s, 0.0, 0.0, 0.5)
 gmsh.model.occ.synchronize()
 gmsh.model.mesh.generate(2)
 
-nodeTags, nodeCoords, _ = gmsh.model.mesh.getNodes(includeBoundary=True)
+nodeTags, nodeCoords, _ = gmsh.model.mesh.getNodes(dim=2, includeBoundary=True)
 _, elemTags, elemNodes = gmsh.model.mesh.getElements(dim=2)
 gmsh.finalize()
 g2i = dict(zip(nodeTags,np.arange(0, nodeTags.shape[0])))
@@ -31,19 +31,19 @@ nodeCoords = nodeCoords.reshape((-1, 3))
 elemNodesIGL = elemNodes[0]
 elemNodesIGL = np.array([g2i[i] for i in elemNodesIGL]).reshape((-1, 3))
 
-# query = np.zeros((n*n*n, 3))
-# for i in range(n):
-#     for j in range(n):
-#         for k in range(n):
-#             query[n*(n*i+j) + k, 0] = -1 + 2.0/n * i
-#             query[n*(n*i+j) + k, 1] = -1 + 2.0/n * j
-#             query[n*(n*i+j) + k, 2] = -1 + 2.0/n * k
-
-query = np.zeros((n*n, 3))
+query = np.zeros((n*n*n, 3))
 for i in range(n):
     for j in range(n):
-        query[(n*i+j), 0] = -1 + 2.0/n * i
-        query[(n*i+j), 1] = -1 + 2.0/n * j
+        for k in range(n):
+            query[n*(n*i+j) + k, 0] = -1 + 2.0/n * i
+            query[n*(n*i+j) + k, 1] = -1 + 2.0/n * j
+            query[n*(n*i+j) + k, 2] = -1 + 2.0/n * k
+
+# query = np.zeros((n*n, 3))
+# for i in range(n):
+#     for j in range(n):
+#         query[(n*i+j), 0] = -1 + 2.0/n * i
+#         query[(n*i+j), 1] = -1 + 2.0/n * j
 
 # IGL winding number
 t0 = perf_counter()
